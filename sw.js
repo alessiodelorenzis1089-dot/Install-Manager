1eae6dfd3a4faaf23466faf51540a2c3314b5e7b
alessiodelorenzis1089-dot <alessiodelorenzis1089@gmail.com>
2026-02-20 11:52:52 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,24 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("install-manager").then(cache => {
+const CACHE_NAME = "install-manager-v1";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
       return cache.addAll([
-        "index.html",
-        "style.css",
-        "script.js",
-        "icon.png"
+        "./",
+        "./index.html",
+        "./style.css",
+        "./script.js",
+        "./icon-192.png",
+        "./icon-512.png"
       ]);
     })
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
